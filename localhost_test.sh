841a2160b10bcbf70aaa1bdd934819c8bfe5d461
Heiko Stamer <HeikoStamer@gmx.net>
2019-08-21 21:02:15 +0200

    use different passwords for each P2P connection

--- localhost_test.sh
+++ localhost_test.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 rm dotsd_localhost*
-dotsd -Y example_policy.html -V -V -P a/a/a/a/ -H localhost0 localhost0 localhost1 localhost2 localhost3 >dotsd_localhost0.stdout 2>dotsd_localhost0.stderr &
-dotsd -V -V -P a/a/a/a/ -H localhost1 localhost0 localhost1 localhost2 localhost3 >dotsd_localhost1.stdout 2>dotsd_localhost1.stderr &
-dotsd -V -V -P a/a/a/a/ -H localhost2 localhost0 localhost1 localhost2 localhost3 >dotsd_localhost2.stdout 2>dotsd_localhost2.stderr &
-dotsd -V -V -P a/a/a/a/ -H localhost3 localhost0 localhost1 localhost2 localhost3 >dotsd_localhost3.stdout 2>dotsd_localhost3.stderr &
+echo "test started"
+dotsd -Y example_policy.html -V -V -P a/b/c/d/ -H localhost0 localhost0 localhost1 localhost2 localhost3 >dotsd_localhost0.stdout 2>dotsd_localhost0.stderr &
+dotsd -Y example_policy.html -V -V -P b/e/f/g/ -H localhost1 localhost0 localhost1 localhost2 localhost3 >dotsd_localhost1.stdout 2>dotsd_localhost1.stderr &
+dotsd -Y example_policy.html -V -V -P c/f/h/i/ -H localhost2 localhost0 localhost1 localhost2 localhost3 >dotsd_localhost2.stdout 2>dotsd_localhost2.stderr &
+dotsd -Y example_policy.html -V -V -P d/g/i/j/ -H localhost3 localhost0 localhost1 localhost2 localhost3 >dotsd_localhost3.stdout 2>dotsd_localhost3.stderr &
 tail -f dotsd_localhost0.stderr
 killall dotsd
 echo "test finished"
