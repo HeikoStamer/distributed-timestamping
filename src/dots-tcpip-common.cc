f53d31893692244a3b8ddab925a1c50c892d63ba
Heiko Stamer <HeikoStamer@gmx.net>
2019-10-20 12:42:28 +0200

    cosmetic change

--- src/dots-tcpip-common.cc
+++ src/dots-tcpip-common.cc
@@ -413,7 +413,7 @@ static int tcpip_mhd_callback
 			tmp << std::endl;
 			const union MHD_DaemonInfo *info = MHD_get_daemon_info(tcpip_mhd,
 				MHD_DAEMON_INFO_CURRENT_CONNECTIONS);
-			tmp << info->num_connections << " connections" << std::endl;
+			tmp << info->num_connections << " connection(s)" << std::endl;
 			std::string page = tmp.str();
 			res = MHD_create_response_from_buffer(page.length(),
 				(void*)page.c_str(), MHD_RESPMEM_MUST_COPY);
